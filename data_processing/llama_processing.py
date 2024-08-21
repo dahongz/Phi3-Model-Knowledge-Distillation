93b5a9f4402f23e4aad2b12b274037fc4e956d26
zhangdah <dahong.zhang@outlook.com>
2024-08-21 18:31:17 +0800

    new

---
+++ data_processing/llama_processing.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import re
+
+from langchain.llms import LlamaCpp
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+
+
+def __create_dataset(data, text_file, model_chain):
+    for i, ind in enumerate(data.index.tolist()):
+        doc = data.loc[ind]['docs'].encode('ascii', errors='ignore').decode('utf-8')
+
+        answer = model_chain.run(doc)
+        answer = answer.replace('\"', '').replace('\n', '')
+        answer = re.split('\d*\.\s', answer)[1:]
+
+        n = len(answer)
+
+        for j in range(n // 2):
+            text_file.write(f'{i + 1} $:$ {answer[j]} $:$ {answer[n - j - 1]}\n')
+
+        print(f'created pairs for doc {ind}/{len(data)}\n')
+
+
+def create_llama_samples(llm_path, docs_path):
+    template = """
+
+        Question: Which are the ten most relevant searching queries in English for this text: {question}? List them.
+        Answer:
+
+        """
+
+    prompt = PromptTemplate(template=template, input_variables=["question"])
+    llm = LlamaCpp(model_path=llm_path)
+    llm_chain = LLMChain(prompt=prompt, llm=llm)
+
+    docs = pd.read_csv(docs_path)
+
+    save_path = './data/llama_samples.txt'
+    with open(save_path, 'a') as f:
+        __create_dataset(docs, f, llm_chain)
+
+
+def llama_samples_to_csv():
+    print('converting to csv...\n')
+    ids = []
+    first = []
+    second = []
+
+    path = './data/llama_samples.txt'
+
+    with open(path) as f:
+        lines = f.readlines()
+
+        for i, line in enumerate(lines):
+
+            line = re.sub(r'http\S+', '', line, flags=re.MULTILINE)
+            line = re.sub(r"[\([{<>})\]\/\"\']", "", line)
+
+            splitted_line = line.split('$:$')
+
+            for j in range(len(splitted_line)):
+                splitted_line[j] = splitted_line[j].strip()
+                splitted_line[j] = splitted_line[j].replace('\n', '')
+
+            if splitted_line[1] != '' and splitted_line[2] != '':
+                ids.append(int(splitted_line[0]))
+                first.append(splitted_line[1])
+                second.append(splitted_line[2])
+
+                print(splitted_line)
+
+    d = {'ids': ids, 'first': first, 'second': second}
+
+    save_path = './data/llama_train.csv'
+    train = pd.DataFrame.from_dict(d)
+    train.to_csv(save_path, index=False)
---
+++ train/Train.py
@@ -0,0 +1,101 @@
+from model.Model import STSBertModel
+from data_processing.Dataset import DataSequence, collate_fn
+
+from transformers import BertTokenizer, XLMRobertaTokenizer, AutoTokenizer
+from transformers.models.deberta_v2 import DebertaV2TokenizerFast
+
+import torch
+import torch.nn as nn
+from torch.optim import Adam
+from torch.utils.data import DataLoader
+
+from tqdm import tqdm
+import pandas as pd
+
+from datetime import datetime
+import yaml
+
+models = {'bert': 'bert-base-uncased',
+          'xlmroberta': 'xlm-roberta-base',
+          'debertav3': 'microsoft/deberta-v3-base',
+          'Phi-3': 'microsoft/Phi-3-mini-4k-instruct'}
+
+model_tokenizer = {'bert-base-uncased': BertTokenizer,
+                   'xlm-roberta-base': XLMRobertaTokenizer,
+                   'microsoft/deberta-v3-base': DebertaV2TokenizerFast,
+                   'microsoft/Phi-3-mini-4k-instruct': AutoTokenizer}
+
+
+def train_and_save(lm, train_path):
+    with open('./config/model.yml', 'r') as stream:
+        try:
+            cfg = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+    bs = cfg['batch_size']
+    epochs = cfg['epochs']
+    learning_rate = float(cfg['learning_rate'])
+
+    use_cuda = torch.cuda.is_available()
+    print(torch.cuda.get_device_name(0))
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    model_name = models[lm]
+    tokenizer = model_tokenizer[model_name].from_pretrained(model_name)
+    dataset = pd.read_csv(train_path)
+
+    model = STSBertModel(model_name) # student
+    model.to(device)
+
+    criterion = nn.CosineEmbeddingLoss()
+    optimizer = Adam(model.parameters(), lr=learning_rate)
+
+    train_dataset = DataSequence(dataset, tokenizer)
+    train_dataloader = DataLoader(train_dataset, num_workers=4, batch_size=bs, shuffle=True)
+    train_loop(model, train_dataloader, device, epochs, optimizer, criterion)
+
+
+def train_loop(model, dataloader, device, epochs, optimizer, criterion):
+    now = datetime.now()
+    best_loss = 1000
+    lossess = []
+
+    for i in range(epochs):
+        model.train()
+
+        total_loss_train = 0.0
+
+        for train_data, train_label in tqdm(dataloader):
+            print(train_data, train_label)
+
+            train_label = torch.tensor(train_label).to(device)
+            train_data['input_ids'] = train_data['input_ids'].to(device)
+            train_data['attention_mask'] = train_data['attention_mask'].to(device)
+            if 'token_type_ids' in train_data.keys():
+                del train_data['token_type_ids']
+
+            train_data = collate_fn(train_data)
+
+            optimizer.zero_grad()
+
+            output = [torch.as_tensor(model(feature)['sentence_embedding'], dtype=torch.float) for feature in
+                      train_data]
+
+            output_1 = torch.stack([out[0] for out in output])
+            output_2 = torch.stack([out[1] for out in output])
+
+            loss = criterion(output_1, output_2, train_label)
+            total_loss_train += loss.item()
+            loss.backward()
+            optimizer.step()
+
+        print(f'Epochs: {i + 1} | Loss: {loss.item()}')
+
+        lossess.append(loss.item())
+
+        if loss.item() < best_loss:
+            best_loss = loss.item()
+            save_path = f'./checkpoints/{model.model_name} : {now.strftime("%m-%d-%Y-%H-%M-%S")}'
+            torch.save(model, save_path)
+            print(f'Best model saved to! {save_path}')
